1ba20d47f306f70c17d138183096cc33647b8c43
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2021-03-18 09:40:09 +0000

    set up of ML library

---
+++ MachineLearning/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace MachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
