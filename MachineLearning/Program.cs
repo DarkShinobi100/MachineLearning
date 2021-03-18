cbd08b56e8f6f9705464e66e68df3452221f4bdd
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2021-03-18 09:46:58 +0000

    flower inputs and outputs created

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -12,3 +12,31 @@ namespace MachineLearning
         }
     }
 }
+//input data from file
+public class FlowerData
+{
+    [LoadColumn(0)]
+    public float SepalLength { get; set; }
+
+    [LoadColumn(1)]
+    public float SepalWidth { get; set; }
+
+    [LoadColumn(2)]
+    public float PetalLength { get; set; }
+
+    [LoadColumn(3)]
+    public float PetalWidth { get; set; }
+
+    [LoadColumn(4)]
+    public string Species { get; set; }
+}
+
+//output data
+public class FlowerPrediction
+{
+    [ColumnName("PredictedLabel")]
+    public string Flower { get; set; }
+
+    [ColumnName("Score")]
+    public float[] Scores { get; set; }
+}
